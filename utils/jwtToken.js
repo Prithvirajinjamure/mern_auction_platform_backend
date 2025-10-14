87eadc75e768d97185dd454151b53321fad8e419
PRITHVIRAJ INJAMURE <prithvirajinjamure@gmail.com>
2025-10-14 14:35:40 +0530

    Add files via upload

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,17 +1,19 @@
 export const generateToken = (user, message, statudCode, res) => {
+        const token = user.generateJsonWebToken();
+        const isProd = process.env.NODE_ENV === 'production';
 
-    const token = user.generateJsonWebToken();
-
-    res.status(statudCode).cookie("token", token, {
-        expires : new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly : true,
-       })
-
-    .json({
-        success : true,
-        message,
-        user,
-        token,
-    });
-          
+        res
+            .status(statudCode)
+            .cookie("token", token, {
+                expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+                httpOnly: true,
+                sameSite: isProd ? 'none' : 'lax',
+                secure: isProd, // required by browsers when sameSite is none
+            })
+            .json({
+                success: true,
+                message,
+                user,
+                token,
+            });
 };  
